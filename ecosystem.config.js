9d54fd5fa8c83373d271e8faa32a6250c911f8ea
pgd988 <pgd988@gmail.com>
2018-10-15 17:02:04 +0300

    develop.json config was added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,6 +11,12 @@ module.exports = {
       env_production: {
         PORT: 8080,
         NODE_ENV: 'production'
+        NODE_CONFIG_ENV: 'production'
+      },
+      env_develop: {
+        PORT: 8080,
+        NODE_ENV: 'development'
+        NODE_CONFIG_ENV: 'develop'
       },
       env_stage: {
         PORT: 8080,
