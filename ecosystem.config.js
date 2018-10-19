af28bcbfa2f368c21e28990a60f33c5e0653b3b2
pgd988 <pgd988@gmail.com>
2018-10-19 10:52:13 +0300

    startup script was changed /4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'middleware-bitcoin-blockprocessor',
-      script: 'index.js',
+      script: './index.js',
       watch: true,
       env: {
         PORT: 8080,
