50c820fb3a047ffe5ef2678169ce747dc9119d2d
pgd988 <pgd988@gmail.com>
2018-10-22 10:41:29 +0300

    ecosystem was changed /2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,12 @@
+const env = {
+  MONGO_URI: 'mongodb://middleware-testnet:ChR0mOn1T6@middleware-bitcoin-db-mongodb.middleware:27017/testnet-bitcoin-middleware-chronobank-io',
+  MONGO_COLLECTION_PREFIX: 'bitcoin',
+  RABBIT_URI: 'amqp://rabbit:nC865xfpbU41@rabbitmq-rabbitmq-svc.default:5672',
+  RABBIT_SERVICE_NAME: 'testnet-bitcoin-middleware-chronobank-io',
+  NETWORK: 'testnet',
+  PROVIDERS: 'http://bcoin-testnet-node.default:18332@tcp://bcoin-testnet-node.default:43332',
+  REST_PORT: 8080
+};
 const env_develop = {
   MONGO_URI: 'mongodb://middleware-testnet:ChR0mOn1T6@middleware-bitcoin-db-mongodb.middleware:27017/testnet-bitcoin-middleware-chronobank-io',
   MONGO_COLLECTION_PREFIX: 'bitcoin',
